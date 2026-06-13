31d6ea9863f3062d377fb5879ba0648ff15de558
Sguinza <cguinda00@gmail.com>
2026-06-13 19:23:29 +0200

    added button for download, to be added : custom location for download, type of bulk download

--- MTGManager.py
+++ MTGManager.py
@@ -1,15 +1,23 @@
 import requests
-import streamlit
+import streamlit as st
+import pandas as pd
+
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
 
 headers = {
-    'User-Agent': 'MTGTool',
-    'From': 'cguinda00@gmail.com'  # This is another valid field
+    'User-Agent': 'MTGTool'
 }
 
-x = requests.get('https://api.scryfall.com/bulk-data/oracle-cards', headers=headers)
-download_String = x.json()['download_uri']
+def downloadDB (type):
+    
+    x = requests.get('https://api.scryfall.com/bulk-data/' + type, headers=headers)
+    download_String = x.json()['download_uri']
+    response = requests.get(download_String, headers=headers)
 
-response = requests.get(download_String, headers=headers)
+    with open ("C:\\temp\\export.json", 'wb') as file:
+        file.write(response.content)
 
-with open ("export.json", 'wb') as file:
-    file.write(response.content)
+st.button("download", on_click=downloadDB, args=["oracle-cards"])
